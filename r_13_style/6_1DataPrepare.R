885899f4bfdb36f3fe5dd76dd3b665d67a0b060a
Jax-Guo <13301132691@163.com>
2017-05-11 23:21:40 +0800

    Data Preparation

--- r_13_style/6_1DataPrepare.R
+++ r_13_style/6_1DataPrepare.R
@@ -1,4 +1,38 @@
+#查看缺失值信息
 install.packages("mice")
 library(mice)
+library(VIM)
 data(sleep,package = "VIM")
 md.pattern(sleep)
+aggr(sleep,prop=F,numbers=T)
+
+#缺失值差补
+install.packages("missForest")
+library(missForest)
+z <- missForest(airquality,verbose = T)
+airquality_full <- z$ximp
+
+#异常值监测
+install.packages("qcc")
+library(qcc)
+data(orangejuice)
+attach(orangejuice)
+qcc(D[trial], sizes=size[trial], type="p")
+
+x <-c(1:100,200)
+(b1 <- boxplot.stats(x))
+
+iris1 <- iris[,1:4]
+kmeans.result <- kmeans(iris1,3)
+kmeans.result
+centers <- kmeans.result$centers[kmeans.result$cluster,]
+distances <- sqrt(rowSums((iris1 - centers)^2))
+outliers <- order(distances,decreasing = T)[1:5]
+print(iris1[outliers,])
+plot(iris1[,c("Sepal.Length","Sepal.Width")],pch="o",
+     col=kmeans.result$centers,cex=0.3)
+# 画出类中心
+points(kmeans.result$centers[,c("Sepal.Length","Sepal.Width")],col=1:3,
+       pch=8,cex=1.5)
+# 画出离群点
+points(iris1[outliers,c("Sepal.Length","Sepal.Width")],pch="+",col=4,cex=1.5)
